c0f8988397e9bb4ca9f1c03dd40b3d9f630320f5
Elijah Walker <advancedathletix@gmail.com>
2022-02-08 09:18:38 -0500

    adding in place holder for exchange calculator and variables in java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,8 @@
 // Api key for exchange rate
 var apiKey ="7ba4330f269157323b1e1f6f"
 
+var currencyEl_one = document.getElementById("currency-one");
+var currencyEl_two = document.getElementById("currency-two");
+var amountE1_one = document.getElementById("amount-one");
+var amountE1_one = document.getElementById("amount-two");
+var rateEl = documnent.getElementById("rate");
